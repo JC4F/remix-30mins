cfdf27411eebeda9dc593f2226abb642945acd4b
jc4f <lequan12122002@gmail.com>
2024-09-25 11:24:31 +0700

    create contact

--- app/root.tsx
+++ app/root.tsx
@@ -12,9 +12,12 @@ import {
 } from "@remix-run/react";
 
 import appStylesHref from "./app.css?url";
-import { getContacts } from "./data";
+import { createEmptyContact, getContacts } from "./data";
 
-// existing exports
+export const action = async () => {
+  const contact = await createEmptyContact();
+  return json({ contact });
+};
 
 export const loader = async () => {
   const contacts = await getContacts();
